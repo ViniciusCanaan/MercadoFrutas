5b35513ce916642e2abca27d4a96ff821c7eceef
Vinicius Canaan <60820733+ViniciusCanaan@users.noreply.github.com>
2021-12-08 19:07:00 -0300

    Criando valores dos produtos e Recibo
    Criando os valores dos produtos, fazendo a soma do total e exibindo no recibo

--- screens/Carrinho.js
+++ screens/Carrinho.js
@@ -1,13 +1,36 @@
 import React, { useEffect, useState, useLayoutEffect } from 'react';
 import {
     useNavigation,
+    useRoute
 } from '@react-navigation/native';
-import { StyleSheet, Text, View, Image, Alert, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, Alert, Button, Modal, Pressable } from 'react-native';
 import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import { colors } from '../styles/variables';
 import { AbacaxiIcon, BananaIcon, MangaIcon, MaçaIcon, PeraIcon } from '../components/Icons';
+import logo from '../assets/logo.png';
 
 const Carrinho = () => {
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const contMaça = route.params.contMaça;
+    const contPera = route.params.contPera;
+    const contBanana = route.params.contBanana;
+    const contAbacaxi = route.params.contAbacaxi;
+    const contManga = route.params.contManga;
+
+
+    const valorAbacaxi = route.params.valorAbacaxi;
+    const valorMaça = route.params.valorMaça;
+    const valorPera = route.params.valorPera;
+    const valorBanana = route.params.valorBanana;
+    const valorManga = route.params.valorManga;
+
+    const totalDoPedido = (valorAbacaxi + valorMaça + valorPera + valorBanana + valorManga)
+
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [showMaçaInPdf, setShowMaçaInPdf] = useState(false);
 
     function handleAddCupom() {
         Alert.alert(
@@ -19,65 +42,117 @@ const Carrinho = () => {
         );
     }
 
-    function handleConfirmPay() {
-        Alert.alert(
-            "Podemos fechar seu pedido?",
-            "Você tem certeza da sua compra?",
-            [
-                {
-                    text: "Cancelar",
-                    onPress: () => console.log("Cancelou Pedido"),
-                    style: "cancel"
-                },
-                { text: "Comprar", onPress: () => console.log("Pedido Efetuado") }
-            ]
-        );
+    function handleAlertPay(){
+        if(totalDoPedido === 0){
+          Alert.alert('Opa','Você não tem nada pra pagar.')
+        }else{
+            Alert.alert(
+                "Podemos fechar seu pedido?",
+                "Você tem certeza da sua compra?",
+                [
+                    {
+                        text: "Cancelar",
+                        onPress: () => console.log("Cancelou Pedido"),
+                        style: "cancel"
+                    },
+                    { text: "Comprar", onPress: (handleConfirmPay)}
+                ]
+            );
+        }
+       
     }
+
+    function handleConfirmPay() {
+        setModalVisible(true);
+       
+    }
+
+    function handleGoHome(){
+        setModalVisible(false);
+        navigation.navigate('Home')
+    }
+
 
     return (
         <View style={styles.viewResumeFruits}>
-            <View style={styles.viewTextCompras}>
-                <Text style={styles.textResumeFruits}>Olá, aqui é o resumo do que está no seu carrinho de compras!!</Text>
-            </View>
-                <View style={styles.fruitSelection}>
-                    <Image source={MaçaIcon} />
-                    <Text style={styles.textFruit}>Maçã</Text>
-                    <View style={styles.circleNumberMaça}>
-                        <Text style={styles.textNumber}></Text>
+            <Modal
+                visible={modalVisible}
+                animationType='fade'
+                transparent={true}
+                onRequestClose={()=> setModalVisible(false)}
+            >
+                <View style={styles.viewModal}>
+                    <View style={styles.conteudoModal}>
+                        <Text style={styles.tituloComprovante}>Comprovante</Text>
+                        <Image source={logo} style={styles.logo} />
                     </View>
-                </View>
-                <View style={styles.fruitSelection}>
-                    <Image source={PeraIcon} />
-                    <Text style={styles.textFruit}>Pêra</Text>
-                    <View style={styles.circleNumberPera}>
-                        <Text style={styles.textNumber}></Text>
+                    
+                    <View style={styles.viewSubtitulo}>
+                        <Text style={styles.subtituloComprovante}>Segue o resumo das suas compras:</Text>
+                        <Text style={styles.subtituloComprovante}>Maçã: R$ {valorMaça}</Text>
+                        <Text style={styles.subtituloComprovante}>Pêra: R$ {valorPera}</Text>
+                        <Text style={styles.subtituloComprovante}>Banana : R$ {valorBanana}</Text>
+                        <Text style={styles.subtituloComprovante}>Abacaxi : R$ {valorAbacaxi}</Text>
+                        <Text style={styles.subtituloComprovante}>Manga : R$ {valorManga}</Text>
+                        <Text style={styles.valorNotaFiscal}>Valor : R$ {totalDoPedido.toFixed(2)}</Text>
+
                     </View>
-                </View>
-                <View style={styles.fruitSelection}>
-                    <Image source={BananaIcon} />
-                    <Text style={styles.textFruit}>Banana</Text>
-                    <View style={styles.circleNumberBanana}>
-                        <Text style={styles.textNumber}></Text>
-                    </View>
-                </View>
-                <View style={styles.fruitSelection}>
-                    <Image source={AbacaxiIcon} />
-                    <Text style={styles.textFruit}>Abacaxi</Text>
-                    <View style={styles.circleNumberAbacaxi}>
-                        <Text style={styles.textNumber}></Text>
-                    </View>
-                </View>
-                <View style={styles.fruitSelection}>
-                    <Image source={MangaIcon} />
-                    <Text style={styles.textFruit}>Manga</Text>
-                    <View style={styles.circleNumberManga}>
-                        <Text style={styles.textNumber}></Text>
+                    <View style={styles.viewPayments}>
+                        <Button
+                        title='Fechar'
+                        onPress={handleGoHome}
+                        />
                     </View>
                 </View>
 
+            </Modal>
+            <View style={styles.viewTextCompras}>
+                <Text style={styles.textResumeFruits}>Olá, aqui é o resumo do que está no seu carrinho de compras!!</Text>
+            </View>
+            <View style={styles.fruitSelection}>
+                <Image source={MaçaIcon} />
+                <Text style={styles.textFruit}>Maçã</Text>
+                <View style={styles.circleNumberMaça}>
+                    <Text style={styles.textNumber}>{contMaça}</Text>
+                </View>
+                <Text>R${valorMaça}</Text>
+            </View>
+            <View style={styles.fruitSelection}>
+                <Image source={PeraIcon} />
+                <Text style={styles.textFruit}>Pêra</Text>
+                <View style={styles.circleNumberPera}>
+                    <Text style={styles.textNumber}>{contPera}</Text>
+                </View>
+                <Text>R${valorPera}</Text>
+            </View>
+            <View style={styles.fruitSelection}>
+                <Image source={BananaIcon} />
+                <Text style={styles.textFruit}>Banana</Text>
+                <View style={styles.circleNumberBanana}>
+                    <Text style={styles.textNumber}>{contBanana}</Text>
+                </View>
+                <Text>R${valorBanana}</Text>
+            </View>
+            <View style={styles.fruitSelection}>
+                <Image source={AbacaxiIcon} />
+                <Text style={styles.textFruit}>Abacaxi</Text>
+                <View style={styles.circleNumberAbacaxi}>
+                    <Text style={styles.textNumber}>{contAbacaxi}</Text>
+                </View>
+                <Text>R${valorAbacaxi}</Text>
+            </View>
+            <View style={styles.fruitSelection}>
+                <Image source={MangaIcon} />
+                <Text style={styles.textFruit}>Manga</Text>
+                <View style={styles.circleNumberManga}>
+                    <Text style={styles.textNumber}>{contManga}</Text>
+                </View>
+                <Text>R${valorManga}</Text>
+            </View>
+
             <View style={styles.viewStepPayments}>
                 <View style={styles.viewResumeCount}>
-                    <Text style={styles.textMoney}>Total do pedido: </Text>
+                    <Text style={styles.textMoney}>Total do pedido: R$ {totalDoPedido.toFixed(2)}  </Text>
                 </View>
                 <View style={styles.viewCupom}>
                     <Text style={styles.textSubtitleMoney}>Possui cupom ou vale?</Text>
@@ -90,7 +165,7 @@ const Carrinho = () => {
                     </TouchableOpacity>
                 </View>
                 <View style={styles.viewPayments}>
-                    <TouchableOpacity style={styles.buttonPay} onPress={handleConfirmPay}>
+                    <TouchableOpacity style={styles.buttonPay} onPress={handleAlertPay}>
                         <Text style={styles.textButton}>Realizar Pedido</Text>
                     </TouchableOpacity>
 
@@ -233,5 +308,51 @@ const styles = StyleSheet.create({
         flex: 2,
         marginTop: 10,
         borderRadius: 10
-    }
+    },
+    viewModal: {
+        width: '90%',
+        height: '80%',
+        backgroundColor: '#B0C4DE',
+        alignSelf: 'center',
+        marginTop: 30
+    },
+    conteudoModal: {
+        alignItems: 'center',
+    },
+    tituloComprovante: {
+        marginTop: 10,
+        fontSize: 20,
+        color: colors.black,
+        fontWeight: 'bold'
+    },
+    viewSubtitulo: {
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start'
+    },
+    subtituloComprovante: {
+        marginTop: 10,
+        fontSize: 16,
+        color: colors.black,
+        paddingLeft: 10
+    },
+    botaoVoltarInicio: {
+        backgroundColor: colors.success,
+        width: '50%',
+        height: 50,
+        borderRadius: 20
+    },
+    viewBotaoVoltarInicio: {
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    valorNotaFiscal:{
+        fontSize: 20,
+        marginTop: 15,
+        color: colors.action,
+        fontWeight: 'bold'
+    },
+    logo: {
+        width: '50%',
+        height: 150
+      },
 });
--- screens/Home.js
+++ screens/Home.js
@@ -17,27 +17,45 @@ const Home = () => {
     const [contManga, setContManga] = useState(0);
     const [mostrarCampo, setMostrarCampo] = useState(false);
 
+    const [valorAbacaxi, setValorAbacaxi] = useState(0);
+    const [valorMaça, setValorMaça] = useState(0);
+    const [valorPera, setValorPera] = useState(0);
+    const [valorBanana, setValorBanana] = useState(0);
+    const [valorManga, setValorManga] = useState(0)
 
+
+    var precoAbacaxi = 4.50;
+    var precoMaca = 5.80;
+    var precoPera = 3.60;
+    var precoBanana = 3.00;
+    var precoManga = 4.00;
+
+  
+    
     function handleAddAbacaxi() {
         setContAbacaxi(contAbacaxi + 1);
+        setValorAbacaxi(contAbacaxi * precoAbacaxi + precoAbacaxi);
     }
-
 
     function handleAddMaça() {
         setContMaça(contMaça + 1);
+        setValorMaça(contMaça * precoMaca + precoMaca);
     }
 
 
     function handleAddPera() {
         setContPera(contPera + 1);
+        setValorPera(contPera * precoPera + precoPera);
     }
 
     function handleAddBanana() {
         setContBanana(contBanana + 1);
+        setValorBanana(contBanana * precoBanana + precoBanana);
     }
 
     function handleAddManga() {
         setContManga(contManga + 1);
+        setValorManga(contManga * precoManga + precoManga);
     }
 
     ////////////////////////////////////
@@ -45,6 +63,7 @@ const Home = () => {
     function handleRemoveAbacaxi() {
         if(contAbacaxi >=1){
             setContAbacaxi(contAbacaxi - 1);
+            setValorAbacaxi(contAbacaxi * precoAbacaxi - precoAbacaxi);
         }else{
             Alert.alert(`Ooops`,`Você já retirou todas as unidades desse produto`)
         }
@@ -54,6 +73,7 @@ const Home = () => {
     function handleRemoveMaça() {
         if(contMaça >=1){
             setContMaça(contMaça - 1);
+            setValorMaça(contMaça * precoMaca - precoMaca);
         }else{
             Alert.alert(`Ooops`,`Você já retirou todas as unidades desse produto`)
         }  
@@ -63,6 +83,7 @@ const Home = () => {
     function handleRemovePera() {
         if(contPera >=1){
             setContPera(contPera - 1);
+            setValorPera(contPera * precoPera - precoPera);
         }else{
             Alert.alert(`Ooops`,`Você já retirou todas as unidades desse produto`)
         }  
@@ -71,6 +92,7 @@ const Home = () => {
     function handleRemoveBanana() {
         if(contBanana >=1){
             setContBanana(contBanana - 1);
+            setValorBanana(contBanana * precoBanana - precoBanana);
         }else{
             Alert.alert(`Ooops`,`Você já retirou todas as unidades desse produto`)
         }  
@@ -79,6 +101,7 @@ const Home = () => {
     function handleRemoveManga() {
         if(contManga >= 1 ){
             setContManga(contManga - 1);
+            setValorManga(contManga * precoManga - precoManga);
         }else{
             Alert.alert(`Ooops`,`Você já retirou todas as unidades desse produto`)
         } 
@@ -111,8 +134,19 @@ const Home = () => {
         setContManga(contAbacaxi === 0);
     }
 
-    function openCart() {
-        navigation.navigate('Carrinho')
+    function handleAddToCart(){
+        navigation.navigate('Carrinho',{
+            contMaça : contMaça,
+            contPera : contPera,
+            contBanana : contBanana,
+            contAbacaxi : contAbacaxi,
+            contManga : contManga,
+            valorAbacaxi: valorAbacaxi,
+            valorMaça: valorMaça,
+            valorPera : valorPera,
+            valorBanana : valorBanana,
+            valorManga : valorManga
+        })
     }
 
     useLayoutEffect(() => {
@@ -146,6 +180,7 @@ const Home = () => {
                     <View style={styles.circleNumberMaça}>
                         <Text style={styles.textNumber}>{contMaça}</Text>
                     </View>
+                    <Text>R${valorMaça}</Text>
                 </View>   
 
                 <View style={styles.fruitSelection}>
@@ -160,6 +195,7 @@ const Home = () => {
                     <View style={styles.circleNumberPera}>
                         <Text style={styles.textNumber}>{contPera}</Text>
                     </View>
+                    <Text>R${valorPera}</Text>
                 </View>
            
                 <View style={styles.fruitSelection}>
@@ -174,6 +210,7 @@ const Home = () => {
                     <View style={styles.circleNumberBanana}>
                         <Text style={styles.textNumber}>{contBanana}</Text>
                     </View>
+                    <Text>R${valorBanana}</Text>
                 </View>
            
                 <View style={styles.fruitSelection}>
@@ -188,6 +225,7 @@ const Home = () => {
                     <View style={styles.circleNumberAbacaxi}>
                         <Text style={styles.textNumber}>{contAbacaxi}</Text>
                     </View>
+                    <Text>R${valorAbacaxi}</Text>
                 </View>
             
                 <View style={styles.fruitSelection}>
@@ -202,6 +240,7 @@ const Home = () => {
                     <View style={styles.circleNumberManga}>
                         <Text style={styles.textNumber}>{contManga}</Text>
                     </View>
+                    <Text>R${valorManga}</Text>
                 </View>
             
             <View style={styles.viewButton}>
@@ -209,14 +248,15 @@ const Home = () => {
                     <Image source={LixeiraIcon} />
                     <Text style={styles.textButton}>Limpar Tudo</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.buttonAddToCart}>
+                <TouchableOpacity style={styles.buttonAddToCart} onPress={handleAddToCart}>
                     <Image source={CarrinhoIcon} />
                     <Text style={styles.textButton}>Colocar no carrinho</Text>
                 </TouchableOpacity>
 
             </View>
+            {/* <Text>{valorCompraTotal}</Text> */}
             <View style={styles.viewButtonCart}>
-                <TouchableOpacity style={styles.goToCartButton} onPress={openCart}>
+                <TouchableOpacity style={styles.goToCartButton} onPress={handleAddToCart}>
                     <Image source={CarrinhoIcon} />
                 </TouchableOpacity>
             </View>
